22df796d6cb1894985341a8c31365b1f234b1621
Diego Isunza <contacto.diegoisunza@gmail.com>
2023-02-26 12:47:57 -0600

    Add assets, components folder

---
+++ components/NavBar.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes'; // for light/dark mode
+import Image from 'next/image'; // Next JS improved Image tag
+import Link from 'next/link'; // Next JS <a/> substitution
+import images from '../assets';
+
+const NavBar = () => (
+  <div>NavBar</div>
+);
+
+export default NavBar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { NavBar, Footer } from '../components';
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white
+    min-h-screen"
+    >
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/e6c7b68c60.js" crossorigin="anonymous" />
+  </ThemeProvider>
+);
+
+export default MyApp;
